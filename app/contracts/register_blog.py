d1a7cc255ff5017043d3dc4200e0b626044006f9
cipherat <me@cipherat.com>
2025-12-14 16:46:23 +0300

    edit: change request fields

--- app/contracts/register_blog.py
+++ app/contracts/register_blog.py
@@ -3,8 +3,8 @@ from typing import List, Optional
 from pydantic import BaseModel, Field
 from datetime import date
 
-from app.contracts.enums import BlogState
-from app.contracts.dates_model import DatesModel
+from app.enums.enums import BlogState
+from app.models.dates import DatesModel
 
 
 class RegisterBlogRequest(BaseModel):
@@ -15,9 +15,9 @@ class RegisterBlogRequest(BaseModel):
     title: str = Field(..., description="The title of the blog post.")
     category: str = Field(..., description="The primary category.")
     keywords: List[str] = Field(..., description="List of keywords/tags.")
-    dates: DatesModel = Field(..., description="Creation, publication, and update dates.")
+    published_at: date = Field(..., description="The publication date.")
     
-    content: str = Field(..., description="The server-side path or URL to the content markdown file.")
+    content_file: str = Field(..., description="The server-side path or URL to the content markdown file.")
     references: List[str] = Field(default_factory=list, description="List of server-side paths or URLs to reference files.")
     
     state: BlogState = Field(..., description="The publication state (drafted or published).")
