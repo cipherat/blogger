0f9c9ac84222f02292ba39fa40782f8043b380e5
cipherat <me@cipherat.com>
2025-12-16 03:17:29 +0300

    add: get by date & blog pagination

--- app/models/blog.py
+++ app/models/blog.py
@@ -7,6 +7,8 @@ from app.enums.enums import BlogState
 from app.models.dates import DatesModel
 from app.repositories.blogs import BlogRepository, get_blog_repository
 
+from app.utils.slugify import slugify
+
 # TODO: combine both classes (attributes & functions), and create another abstracted
 # class for Blog(...) to inherit, such that the abstraction initializes the repository
 # connection and Blog() can `self.repository` that while implementing the inherited functions
@@ -20,9 +22,27 @@ class BlogModel(BaseModel):
     references: List[str]
     state: BlogState
 
+    @property
+    def permalink_key(self) -> str:
+        publish_date = self.dates.published_at or self.dates.created_at
+        
+        year = publish_date.strftime("%Y")
+        month = publish_date.strftime("%m")
+        day = publish_date.strftime("%d")
+        slug = slugify(self.title)
+
+        return f"{year}/{month}/{day}/{slug}"
+
+
 class Blog:
     def __init__(self, repository: BlogRepository):
         self.repository = repository
+
+    def find_by_permalink(self, permalink: str) -> Optional[BlogModel]:
+        data = self.repository.find_by_permalink(permalink)
+        if data:
+            return BlogModel(**data)
+        return None
         
     def save(self, blog_data: BlogModel) -> None:
         self.repository.add_blog(blog_data.model_dump())
@@ -33,6 +53,17 @@ class Blog:
             return BlogModel(**data) 
         return None
 
+    def get_paged_blogs(self, page: int, limit: int) -> Dict[str, Any]:
+        repo_data = self.repository.read_paged_blogs(page, limit)
+        blogs = [BlogModel(**blog) for blog in repo_data.get("blogs")]
+        
+        blogs.sort(key=lambda b: b.dates.published_at or b.dates.created_at, reverse=True)
+
+        return {
+            "blogs": blogs,
+            "total_count": repo_data.get("total_count")
+        }
+    
     def get_all(self) -> List[BlogModel]:
         blogs = self.repository.get_all()
         return [BlogModel(**blog) for blog in blogs]
