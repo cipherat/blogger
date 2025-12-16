41e23f4bef7c9ff0c4aaa83bdbc20568118829b5
cipherat <me@cipherat.com>
2025-12-16 03:16:31 +0300

    add: get by date & blog pagination

--- app/controllers/blog.py
+++ app/controllers/blog.py
@@ -1,13 +1,16 @@
 from typing import List
 
-from fastapi import APIRouter, UploadFile, File, Depends, HTTPException, status
+from fastapi import APIRouter, UploadFile, File, Depends, Request, HTTPException, status, Query
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel, Field
 
 from app.services.get_blog import GetBlogService, get_get_blog_service
+from app.services.get_page_blogs import GetPageBlogsService, get_get_page_blogs_service
 from app.services.get_blogs import GetBlogsService, get_get_blogs_service
 from app.services.register_blog import RegisterBlogService, get_register_blog_service
 
 from app.contracts.get_blog import GetBlogResponse
+from app.contracts.get_page_blogs import GetPageBlogsResponse
 from app.contracts.get_blogs import GetBlogsResponse
 from app.contracts.register_blog import RegisterBlogRequest, RegisterBlogResponse
 
@@ -17,6 +20,66 @@ router = APIRouter(
     tags=["Blog Management"],
 )
 
+templates = Jinja2Templates(directory="templates")
+
+@router.get("/{year}/{month}/{day}/{title}", status_code=status.HTTP_200_OK)
+async def get_blog_by_permalink(
+    year: str,
+    month: str,
+    day: str,
+    title: str,
+    service: GetBlogService = Depends(get_get_blog_service),
+) -> GetBlogResponse:
+    try:
+        result = service.run(year, month, day, title)
+        return GetBlogResponse(
+            status=result.get("status", "empty"),
+            blog=result.get("blog", None),
+            message=result.get("message", f"No blogs found."),
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Internal server error: {e}",
+        )
+
+@router.get("/", name="get_page_blogs", response_model=GetPageBlogsResponse, status_code=status.HTTP_200_OK)
+async def get_page_blogs(
+    request: Request,
+    page: int = Query(1, ge=1, description="..."),
+    limit: int = Query(20, ge=1, le=100, description="..."),
+    service: GetPageBlogsService = Depends(get_get_page_blogs_service),
+) -> GetPageBlogsResponse:
+    try:
+        result = service.run(page, limit)
+
+        context = {
+            "request": request,
+            "blogs": result.get("blogs", []),
+            "page_data": result,
+            "current_page": result.get("page", 1),
+        }
+        
+        return templates.TemplateResponse("index.html", context)
+
+        # return GetPageBlogsResponse(
+        #     status=result.get("status", "empty"),
+        #     blogs=result.get("blogs"),
+        #     total_count=result.get("total_count"),
+        #     page=result.get("page"),
+        #     limit=result.get("limit"),
+        #     total_pages=result.get("total_pages"),
+        #     has_next=result.get("has_next"),
+        #     has_previous=result.get("has_previous"),
+        #     message=result.get("message", "No blogs found (empty result)."),
+        # )
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Internal server error: {e}",
+        )
+    
+"""
 @router.get("/{blog_id}", response_model=GetBlogResponse, status_code=status.HTTP_200_OK)
 async def get_blog_by_id(
     blog_id: str,
@@ -34,8 +97,9 @@ async def get_blog_by_id(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail=f"Internal server error: {e}",
             )
-    
+"""
 
+"""
 @router.get("/", response_model=GetBlogsResponse, status_code=status.HTTP_200_OK)
 async def get_all_blogs(
     service: GetBlogsService = Depends(get_get_blogs_service)
@@ -52,6 +116,7 @@ async def get_all_blogs(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"Internal server error: {e}",
         )
+"""
 
 @router.post("/register", response_model=RegisterBlogResponse, status_code=status.HTTP_201_CREATED)
 async def register_blog(
