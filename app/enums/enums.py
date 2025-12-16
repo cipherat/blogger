1db9a462c6696b373164ab51058cce873511bc73
cipherat <me@cipherat.com>
2025-12-16 03:55:05 +0300

    add: list state values

--- app/enums/enums.py
+++ app/enums/enums.py
@@ -4,3 +4,7 @@ class BlogState(Enum):
     """Corresponds to the 'state' field in your JSON example."""
     drafted = "drafted"
     published = "published"
+
+    @classmethod
+    def list_values(cls):
+        return [state.value for state in cls]
