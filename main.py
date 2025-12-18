79fa3811869295faa4ef1189a2e2c068910db278
cipherat <me@cipherat.com>
2025-12-19 00:48:57 +0300

    add: initiate postgres client with app lifespan

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
+import os
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 
+from contextlib import asynccontextmanager
+
+from app.gateways.postgres.client import PostgresClient
 from app.controllers.blog import router as blog_router
 
 
 API_VERSION = "v1.0.0"
 API_PREFIX = f"/api/{API_VERSION.split('.')[0]}"
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.db_client = PostgresClient(
+        dbname=os.getenv("DB_NAME", "blogger_db"),
+        user=os.getenv("DB_USER", "blogger_user"),
+        password=os.getenv("DB_PASS", "your_password"),
+        host=os.getenv("DB_HOST", "database"),
+        port=os.getenv("DB_PORT", 5432)
+    )
+    yield
+    app.state.db_client.close_all()
+
 app = FastAPI(
     title="Blog Management Tool API",
     version=API_VERSION,
     root_path=API_PREFIX,
+    lifespan=lifespan,
 )
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
